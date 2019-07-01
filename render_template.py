06fd02ba5b3c1146dcde559d7ab3893239317b4e
pengsun <pengsun000@gmail.com>
2019-07-01 15:52:21 +0800

    render_template removes whitespaces

--- render_template.py
+++ render_template.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-""" copied from https://github.com/tensorflow/ecosystem """
+""" modified from https://github.com/tensorflow/ecosystem """
 from __future__ import print_function
 
 import jinja2
@@ -9,4 +9,10 @@ if len(sys.argv) != 2:
   print("usage: {} [template-file]".format(sys.argv[0]), file=sys.stderr)
   sys.exit(1)
 with open(sys.argv[1], "r") as f:
-  print(jinja2.Template(f.read()).render())
+  # print(jinja2.Template(f.read()).render())
+  e = jinja2.Environment(
+    trim_blocks=True  # remove white spaces
+  )
+  t = e.from_string(f.read())
+  print(t.render())
+
